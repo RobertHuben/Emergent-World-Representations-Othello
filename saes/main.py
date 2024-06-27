6eb687f1a4d7b0a4b94d4005628a9869e84767c9
Robert <rvhuben@gmail.com>
2024-06-26 21:28:19 -0400

    rewrote main.py to do a sweep over the data size hyperparameter

--- saes/main.py
+++ saes/main.py
@@ -6,26 +6,21 @@ from utils import load_pre_trained_gpt, load_dataset
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
+def training_dataset_sweep():
+    for entries_limit in [1e3, 1e4,1e5,1e6]:
+        GPT_probe=load_pre_trained_gpt()
+        print("Beginning training process. It may take a moment to load the datasets...")
+        train_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=True, entries_limit=int(entries_limit))
+        test_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=False, entries_limit=1000)
+
+        sae=SAEAnthropic(gpt=GPT_probe, feature_ratio=2, sparsity_coefficient=10, window_start_trim=4, window_end_trim=4)
+
+        print("SAE initialized, proceeding to train!")
+        sae.train_model(train_dataset, test_dataset, report_every_n_steps=500)
+
+        print("Finished SAE training, proceeding to evaluate classifiers!")
+        sae.compute_all_smd(sae, test_dataset)
+        print(sae.model_specs_to_string())
+
 if __name__=="__main__":
-    probe_path = "EWOthello/ckpts/DeanKLi_GPT_Synthetic_8L8H/"
-    probe_layer = 3
-    GPT_probe=load_pre_trained_gpt(probe_path=probe_path, probe_layer=probe_layer)
-
-    print("Beginning training process. It may take a moment to load the datasets...")
-    train_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=True, entries_limit=1000000)
-    test_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=False, entries_limit=1000)
-
-    sae=SAEAnthropic(gpt=GPT_probe, feature_ratio=2, sparsity_coefficient=10, window_start_trim=4, window_end_trim=4)
-
-    print("SAE initialized, proceeding to train!")
-    sae.train_model(train_dataset, test_dataset, report_every_n_steps=500)
-
-    print("Finished SAE training, proceeding to evaluate classifiers!")
-    sae.compute_all_aurocs(sae, test_dataset)
-    good_classifier_count=sae.num_high_accuracy_classifiers()
-    print(f"Number of high-accuracy aurocs: {good_classifier_count}")
-    
-    
-    save_results_location= "best_aurocs.pkl"
-    torch.save(sae.classifier_aurocs, save_results_location)
-    print(f"Saved results to {save_results_location}")
+    training_dataset_sweep()
