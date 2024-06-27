3af4e3002a5a39629e90a45f3dafabf882cd199b
Robert <rvhuben@gmail.com>
2024-06-27 10:40:59 -0400

    rewrote main training loop to use new methods and be simpler

--- saes/main.py
+++ saes/main.py
@@ -2,25 +2,20 @@ import torch
 from tqdm import tqdm
 
 from sae import SAEAnthropic
-from utils import load_pre_trained_gpt, load_dataset
+from utils import load_pre_trained_gpt, load_dataset, load_datasets_automatic
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
 def training_dataset_sweep():
-    for entries_limit in [1e3, 1e4,1e5,1e6]:
-        GPT_probe=load_pre_trained_gpt()
-        print("Beginning training process. It may take a moment to load the datasets...")
-        train_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=True, entries_limit=int(entries_limit))
-        test_dataset = load_dataset(split_fraction=.95, use_first_half_of_split=False, entries_limit=1000)
-
-        sae=SAEAnthropic(gpt=GPT_probe, feature_ratio=2, sparsity_coefficient=10, window_start_trim=4, window_end_trim=4)
-
-        print("SAE initialized, proceeding to train!")
-        sae.train_model(train_dataset, test_dataset, report_every_n_steps=500)
-
-        print("Finished SAE training, proceeding to evaluate classifiers!")
-        sae.compute_all_smd(sae, test_dataset)
-        print(sae.model_specs_to_string())
+    GPT_probe=load_pre_trained_gpt()
+    saes=[]
+    _, smd_evaluation_dataset =load_datasets_automatic(train_size=1, test_size=1000)
+    for train_size in [1e3, 1e4, 1e5, 1e6, 1e7]:
+        train_dataset,test_dataset = load_datasets_automatic(train_size=int(train_size), test_size=1000)
+        saes.append(SAEAnthropic(gpt=GPT_probe, feature_ratio=2, sparsity_coefficient=10, window_start_trim=4, window_end_trim=4))
+        saes[-1].train_model(train_dataset, test_dataset, report_every_n_steps=100000)
+        saes[-1].compute_all_smd(smd_evaluation_dataset)
+        print(saes[-1].model_specs_to_string())
 
 if __name__=="__main__":
     training_dataset_sweep()
