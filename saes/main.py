30fea4d8cb46e6985c7b645d84e24badd2488e9d
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-07-01 09:52:59 -0700

    Moved code to plot smds and aurocs to new file analysis_plotter.py

--- saes/main.py
+++ saes/main.py
@@ -4,12 +4,10 @@ sys.path.append(os.path.join( os.path.dirname ( __file__), os.path.pardir))
 
 import torch 
 from tqdm import tqdm
-import math
-import numpy as np
-from matplotlib import pyplot as plt
 
 from sae import SAEAnthropic, SAEPretrainedProbes
 from utils import load_pre_trained_gpt, load_dataset, load_datasets_automatic
+from analysis_plotter import plot_smd_auroc_distributions
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -25,50 +23,12 @@ def training_dataset_sweep():
         print(saes[-1].model_specs_to_string())
 
 def evaluate_pretrained_probes(save_dir=None):
-    if save_dir:
-        os.makedirs(save_dir, exist_ok=True)
-    metrics = ["smd", "auroc"]
-    thresholds = [2.0, 0.9]
-    _, test_dataset = load_datasets_automatic(train_size=1, test_size=1000)
     gpts_for_probing = {layer:load_pre_trained_gpt(probe_layer=layer) for layer in range(1, 8+1)}
     probes = {layer: SAEPretrainedProbes(gpts_for_probing[layer], probe_layer=layer, window_start_trim=4, window_end_trim=4) for layer in range(1, 8+1)}
     for layer in range(1, 8+1):
-        probes[layer].compute_all_smd(test_dataset)
-        probes[layer].compute_all_aurocs(test_dataset)
-
-        layer_str = f"Layer {layer} classification metrics:\n"
-
-        best_smds = probes[layer].classifier_smds.max(dim=0).values.flatten().detach().numpy()
-        best_aurocs = probes[layer].classifier_aurocs.max(dim=0).values.flatten().detach().numpy()
-
-        for i, array in enumerate([best_smds, best_aurocs]):
-            average = round(probes[layer].average_classifier_score(metric_name=f"classifier_{metrics[i]}s"), 3)
-            above_threshold = probes[layer].num_classifier_above_threshold(metric_name=f"classifier_{metrics[i]}s", threshold=thresholds[i])
-            layer_str += f"Average {metrics[i]}: {average}\nNumber of classifiers with {metrics[i]} > {thresholds[i]}: {above_threshold}\n"
-
-            bins = np.linspace(min(array), max(array), 75) # fixed number of bins
-
-            plt.xlim([0, max(array)+0.5])
-
-            plt.hist(array, bins=bins, alpha=0.5)
-            plt.title(f'{metrics[i]} distribution')
-            plt.xlabel(metrics[i])
-            plt.ylabel('count')
-
-            if save_dir:
-                plt.savefig(f"{save_dir}/layer_{layer}_{metrics[i]}s_distribution.png")
-                plt.close()
-            else:
-                plt.show()
-
-        layer_str += "\n"
-        if save_dir:
-            with open(save_dir+"/classifier_metrics.txt", "a") as f:
-                f.write(layer_str)
-        else:
-            print(layer_str)
-
-
+        layer_dir = f"{save_dir}/layer_{layer}"
+        plot_smd_auroc_distributions(probes[layer], save_dir=layer_dir)
+        
 
 if __name__=="__main__":
 
