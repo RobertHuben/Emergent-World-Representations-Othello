12039d2b94cf061597005386e5c041e6476dbb79
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-07-13 16:03:41 +1000

    test

--- saes/main.py
+++ saes/main.py
@@ -72,7 +72,7 @@ if __name__=="__main__":
     #leaky_topk_training_sweep(k_list=[75, 100], epsilon_list=[0.005], mode_list=["absolute"])
     #gated_training_sweep([60, 100, 120, 150], ["standard"])
 
-    #sae = torch.load("trained_models/07_13_test_sae.pkl", map_location=device)
-    sae = torch.load("trained_models/for_analysis/07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl", map_location=device)
+    sae = torch.load("trained_models/07_13_test_sae.pkl", map_location=device)
+    #sae = torch.load("trained_models/for_analysis/07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl", map_location=device)
     sae_to_probe = SAEforProbing(sae)
     train_probe(sae_to_probe, "test_sae_probe")
