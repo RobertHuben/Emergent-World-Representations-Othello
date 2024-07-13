2c1e5baf3ac01ca8e5fdcb4dc0747a6095d4cbe9
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-07-13 11:15:58 +1000

    debugged LinearProbe

--- saes/probes.py
+++ saes/probes.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.join( os.path.dirname ( __file__), os.path.pardir))
+
 import torch
 from torch.nn import functional as F
 import numpy as np
@@ -25,14 +29,19 @@ class ProbeDataset(Dataset):
         a = OthelloBoardState()
         board_state = a.get_gt(tbf[:valid_until], "get_state")
         board_state = (np.array(board_state) - 1.0) * self.enemy_own_modifier[:valid_until, :] + 1.0
-        
-        return x, torch.tensor(board_state, dtype=torch.float32)
+        board_state = torch.tensor(board_state, dtype=torch.float32)
+        if valid_until < len(tbf):
+            padding = -100*torch.ones(len(tbf)-valid_until, 64).to(device)
+            board_state = torch.cat((board_state, padding), 0)
+        return x, board_state.to(dtype=int)
 
 
 class LinearProbe(torch.nn.Module):
     def __init__(self, model_to_probe:torch.nn.Module, input_dim:int):
+        super().__init__()
         self.model_to_probe = model_to_probe
         self.linear = torch.nn.Linear(input_dim, 64*3)
+        self.num_data_trained_on=0
 
         for param in model_to_probe.parameters():
             param.requires_grad=False
@@ -40,10 +49,11 @@ class LinearProbe(torch.nn.Module):
     def forward(self, inputs, targets=None):
         activations = self.model_to_probe(inputs)
         logits = self.linear(activations)
-        if targets:
-            loss = F.cross_entropy(logits, targets)
-        else:
+        logits = logits.view((logits.shape[0], logits.shape[1], 64, 3))
+        if targets == None:
             loss = None
+        else:
+            loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), targets.view(-1), ignore_index=-100)
         return loss, logits
     
     def train_model(self, train_dataset:CharDataset, eval_dataset:CharDataset, batch_size=64, num_epochs=1, report_every_n_data=500, learning_rate=1e-3, fixed_seed=1337):
@@ -66,8 +76,9 @@ class LinearProbe(torch.nn.Module):
         for epoch in range(num_epochs):
             train_dataloader=iter(torch.utils.data.DataLoader(train_probe_dataset, batch_size=batch_size, shuffle=True))
             print(f"Beginning epoch {epoch+1}/{num_epochs}. Epoch duration is {len(train_dataloader)} steps, will evaluate every {report_every_n_data} games.")
-            
+            batch_count=0 #for testing
             for input_batch, label_batch in tqdm(train_dataloader):
+                batch_count += 1 #for testing
                 input_batch=input_batch.to(device)
                 label_batch=label_batch.to(device)
                 step+=1
@@ -106,7 +117,7 @@ class LinearProbe(torch.nn.Module):
     def compute_accuracy(self, logits, targets):
         predictions = torch.argmax(logits, dim=-1, keepdim=False)
         hits = predictions == targets
-        return hits.mean()
+        return hits.sum()/(targets != -100).sum()
 
     def print_evaluation(self, train_loss, eval_dataset:CharDataset, step_number="N/A"):
         losses, logits, targets=self.catenate_outputs_on_dataset(eval_dataset, include_loss=True)
