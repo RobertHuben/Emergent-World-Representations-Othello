5a66088b8b13b144ebcb69872aadde1e6e276db7
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-08-14 10:49:57 -0700

    test

--- saes/main.py
+++ saes/main.py
@@ -140,8 +140,8 @@ if __name__=="__main__":
     #leaky_topk_training_sweep(k_list=[75, 100], epsilon_list=[0.005], mode_list=["absolute"])
     #gated_training_sweep([60, 100, 120, 150], ["standard"])
 
-    sae_location = "trained_models/for_analysis/07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl"
-    #sae_location = "07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl"
+    #sae_location = "trained_models/for_analysis/07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl"
+    sae_location = "07_09_gated_tied_weights_no_aux_loss_coeff=1.5.pkl"
     
     """ sae = torch.load(sae_location, map_location=device)
     sae_to_probe = SAEforProbing(sae)
@@ -197,8 +197,8 @@ if __name__=="__main__":
     probe = Constant_Probe(sae_to_probe, input_dim=1024)
     train_probe(probe, "constant_probe", train_params=training_params, eval_after=True) """
 
-    L1_probe_location = "trained_probes/08_09_L1_probe___sae=07_09_gated_tied_weights_no_aux_loss_coeff=1.5___coeff=30.pkl"
-    #L1_probe_location = "08_09_L1_probe___sae=07_09_gated_tied_weights_no_aux_loss_coeff=1.5___coeff=30.pkl"
+    #L1_probe_location = "trained_probes/08_09_L1_probe___sae=07_09_gated_tied_weights_no_aux_loss_coeff=1.5___coeff=30.pkl"
+    L1_probe_location = "08_09_L1_probe___sae=07_09_gated_tied_weights_no_aux_loss_coeff=1.5___coeff=30.pkl"
 
     sae = torch.load(sae_location, map_location=device)
     sae_to_probe = SAEforProbing(sae)
