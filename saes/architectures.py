356b960d32d52ee0684229f7c6a39b99ae81f878
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-09-10 09:00:18 -0700

    removed version of gated sae with sigmoid activation

--- saes/architectures.py
+++ saes/architectures.py
@@ -90,13 +90,12 @@ class MultiHeadedTopKSAE(SAETemplate):
 #currently uses tied weights only
 #to try: untied weights original version, as well as using sigmoid instead of step function for training to avoid aux_loss
 class Gated_SAE(SAEAnthropic):
-    def __init__(self, gpt: GPTforProbing, num_features: int, sparsity_coefficient: float, no_aux_loss=False, sigmoid_act_coeff=False, decoder_initialization_scale=0.1):
+    def __init__(self, gpt: GPTforProbing, num_features: int, sparsity_coefficient: float, no_aux_loss=False, decoder_initialization_scale=0.1):
         super().__init__(gpt, num_features, sparsity_coefficient, decoder_initialization_scale)
         self.b_gate = self.encoder_bias #just renaming to make this more clear
         self.r_mag = torch.nn.Parameter(torch.zeros((num_features)))
         self.b_mag = torch.nn.Parameter(torch.zeros((num_features)))
         self.no_aux_loss = no_aux_loss
-        self.sigmoid_act_coeff = sigmoid_act_coeff
 
     def forward(self, residual_stream, compute_loss=False):
         if self.no_aux_loss:
@@ -105,10 +104,6 @@ class Gated_SAE(SAEAnthropic):
             encoder = self.encoder
         encoding = (residual_stream - self.decoder_bias) @ encoder
         if self.no_aux_loss:
-            if self.sigmoid_act_coeff:
-                features_to_use = torch.sigmoid(self.sigmoid_act_coeff*(encoding + self.b_gate))
-                hidden_layer = F.relu(features_to_use * encoding * torch.exp(self.r_mag) + self.b_mag)
-            else:
                 features_to_use = F.relu(encoding + self.b_gate)
                 hidden_layer = F.relu(features_to_use * torch.exp(self.r_mag) + self.b_mag) #is b_mag really necessary here?
         else:
