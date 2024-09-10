267a4a459fc3eed26eecc94bd52228ea444efb2e
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-09-10 15:10:22 -0700

    test

--- saes/main.py
+++ saes/main.py
@@ -132,9 +132,10 @@ if __name__=="__main__":
     
     gpt = load_pre_trained_gpt(probe_layer=3)
     epsilon = 0.01
-    delta = 0.25
-    coeffs = [256, 512]
-    for coeff in coeffs:
+    deltas = [0.5, 1.0, 2.5, 5.0]
+    coeffs = [50, 100, 200, 300]
+    for delta in deltas:
+        for coeff in coeffs:
             sae = Gated_Smoothed_L0_SAE(gpt, 1024, coeff, epsilon, delta)
             sae_name = f"gated_smoothed_L0_coeff={coeff}_delta={delta}_epsilon={epsilon}"
             print(f"Beginning training of {sae_name}")
